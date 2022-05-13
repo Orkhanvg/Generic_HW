8fb72803525d3c82939e983b7e3e771363eb7dc9
orkhanvg <orkhanvg@code.edu.az>
2022-05-13 15:53:48 +0400

    Add project files.

---
+++ Homework_Generic/Models/Group.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Homework_Generic
+{
+    public class Group
+    {
+
+        public static int result { get; set; }
+
+       private  List<Student> students = new List<Student>();
+     
+        public string GroupNo { get; }
+        public Group()
+        {
+           
+            string str = "AP101";
+            string digits = new string(str.Where(char.IsDigit).ToArray());
+            string letters = new string(str.Where(char.IsLetter).ToArray());
+            int num;
+            int.TryParse(digits, out num);
+            result++;
+            num = num + result;
+            digits = Convert.ToString(num);
+            letters = letters + digits;
+            Console.WriteLine(letters);
+
+
+
+           
+
+        }
+
+
+        public void AddStudent(Student stu)
+        {
+            students.Add(stu);
+        }
+
+        public void GetAllStudents()
+        {
+            foreach (Student student in students)
+            {
+                Console.WriteLine($"Student Id:{student.Id}\nName:{student.Name}\nSurname:{student.Surname}\nAge:{student.Age}\nPoint:{student.Point}");
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+        }
+
+        public List<Student> Sort()
+        {
+
+            List<Student> SortedList = students.OrderBy(x => x.Point).ToList();
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine("Sort Point");
+            Console.WriteLine();
+            foreach (Student student in SortedList)
+            {
+
+                Console.WriteLine($"Student Id:{student.Id}\nName:{student.Name}\nSurname:{student.Surname}\nAge:{student.Age}\nPoint:{student.Point}");
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            return SortedList;
+        }
+
+    }
+}
---
+++ Homework_Generic/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Homework_Generic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 1.3 Program class
+            //a) 4 dənə student obyekti yaradın qarışıq point dəyərlərində.
+            //b) Group obyekti yaradın və group obyektinə AddStudent methodundan istifadə edərək studentləri əlavə edin.
+            //c) GetAllStudents methodundan istifadə edərək bütün studentləri ekrana çıxardın.
+            //d) Sort methodundan istifadə edərək sort olunmuş studentləri ekrana çıxardın.
+            //e) İkinci bir Group obyketi yaradın və qrup nömrəsinin avtomatik artmağını yoxlayın.
+
+
+            Student student = new Student("Sahil", "Mamedov", 25, 100);
+
+            Student student2 = new Student("Efqan", "Hesenov", 27, 42);
+            Student student3 = new Student("Orxan", "Salmanov", 25, 60);
+            Student student4 = new Student("Afet", "Salmanova", 25, 33);
+            Console.WriteLine(student > student2);
+            Console.WriteLine(student2 < student);
+            Console.WriteLine(student3 > student4);
+            Console.WriteLine(student3 < student4);
+
+            Group group = new Group(); 
+            group.AddStudent(student);
+            group.AddStudent(student2);
+            group.AddStudent(student3);
+            group.AddStudent(student4);
+            group.GetAllStudents();
+            group.Sort();
+            Group group1 = new Group();
+            Group group2 = new Group();
+            Group group3 = new Group();
+            Group group4 = new Group();
+            Group group5 = new Group();
+            Group group6 = new Group();
+
+
+        }
+    }
+}
